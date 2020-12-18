debcbd97dfceea29a986f4d66bd5aa98b3036345
Andrew Kane <andrew@ankane.org>
2020-12-17 20:47:12 -0800

    Added more features

--- lib/khiva/features.rb
+++ lib/khiva/features.rb
@@ -12,6 +12,48 @@ module Khiva
         FFI.call(:absolute_sum_of_changes, arr, result)
         Array.new(result)
       end
+
+      def maximum(arr)
+        result = Utils.create_ptr
+        FFI.call(:maximum, arr, result)
+        Array.new(result)
+      end
+
+      def mean(arr)
+        result = Utils.create_ptr
+        FFI.call(:mean, arr, result)
+        Array.new(result)
+      end
+
+      def mean_absolute_change(arr)
+        result = Utils.create_ptr
+        FFI.call(:mean_absolute_change, arr, result)
+        Array.new(result)
+      end
+
+      def mean_change(arr)
+        result = Utils.create_ptr
+        FFI.call(:mean_change, arr, result)
+        Array.new(result)
+      end
+
+      def mean_second_derivative_central(arr)
+        result = Utils.create_ptr
+        FFI.call(:mean_second_derivative_central, arr, result)
+        Array.new(result)
+      end
+
+      def median(arr)
+        result = Utils.create_ptr
+        FFI.call(:median, arr, result)
+        Array.new(result)
+      end
+
+      def minimum(arr)
+        result = Utils.create_ptr
+        FFI.call(:minimum, arr, result)
+        Array.new(result)
+      end
     end
   end
 end
--- test/features_test.rb
+++ test/features_test.rb
@@ -2,14 +2,51 @@ require_relative "test_helper"
 
 class FeaturesTest < Minitest::Test
   def test_abs_energy
-    tss = Khiva::Array.new([1, 2, 3, 4, 5], type: :f32)
     expected = [55]
     assert_elements_in_delta expected, Khiva::Features.abs_energy(tss)
   end
 
   def test_absolute_sum_of_changes
-    tss = Khiva::Array.new([1, 2, 3, 4, 5], type: :f32)
     expected = [4]
     assert_elements_in_delta expected, Khiva::Features.absolute_sum_of_changes(tss)
+  end
+
+  def test_maximum
+    expected = [5]
+    assert_elements_in_delta expected, Khiva::Features.maximum(tss)
+  end
+
+  def test_mean
+    expected = [3]
+    assert_elements_in_delta expected, Khiva::Features.mean(tss)
+  end
+
+  def test_mean_absolute_change
+    expected = [0.8]
+    assert_elements_in_delta expected, Khiva::Features.mean_absolute_change(tss)
+  end
+
+  def test_mean_change
+    expected = [0.8]
+    assert_elements_in_delta expected, Khiva::Features.mean_change(tss)
+  end
+
+  def test_mean_second_derivative_central
+    expected = [0]
+    assert_elements_in_delta expected, Khiva::Features.mean_second_derivative_central(tss)
+  end
+
+  def test_median
+    expected = [3]
+    assert_elements_in_delta expected, Khiva::Features.median(tss)
+  end
+
+  def test_minimum
+    expected = [1]
+    assert_elements_in_delta expected, Khiva::Features.minimum(tss)
+  end
+
+  def tss
+    Khiva::Array.new([1, 2, 3, 4, 5], type: :f32)
   end
 end
